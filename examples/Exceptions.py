3d65670cfde62e88369ffd01cf8dc6502f66c96b
angeldaughtry <100389979+angeldaughtry@users.noreply.github.com>
2023-09-25 12:11:21 -0500

    Update Exceptions.py

--- examples/Exceptions.py
+++ examples/Exceptions.py
@@ -7,28 +7,28 @@
 # https://docs.python.org/3.8/library/exceptions.html
 
 def f (b) :
-    if b :
+    if b : #if b is true
         raise NameError("abc")
-    return 0
+    return 0 #if b is false
 
 print("Exceptions.py")
 
 try :
-    assert f(False) == 0
-except NameError :
+    assert f(False) == 0 #put statement we expect to cause trouble bc f(False) returns 0
+except NameError : #this gets skipped
     assert False
 
 try :
-    assert f(True) == 1
-    assert False
-except NameError as e :
+    assert f(True) == 1 #this doesn't happen bc an expection is raised, everything stops at this point and goes to the expection block
+    assert False # does not excecute
+except NameError as e : #alias NameError as e
     assert isinstance(e,      NameError)
     assert isinstance(e.args, tuple)
     assert len(e.args)  ==     1
     assert e.args       is not ("abc",)
     assert e.args       ==     ("abc",)
-else :
-    assert False
+else : # does not execute
+    assert False 
 
 assert isinstance(NameError, type)
 assert isinstance(type,      type)
