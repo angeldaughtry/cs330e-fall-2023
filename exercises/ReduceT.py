465cf72d978950843c37ee937db53c1a5174b511
angeldaughtry <100389979+angeldaughtry@users.noreply.github.com>
2023-10-02 13:35:24 -0500

    Update ReduceT.py

--- exercises/ReduceT.py
+++ exercises/ReduceT.py
@@ -10,33 +10,33 @@ from functools import reduce
 from operator  import add, sub
 from unittest  import main, TestCase
 
-from Reduce import    \
+from Reduce import    \ # the functions that you implement from your file
     reduce_for_range, \
     reduce_for,       \
     reduce_while
 
-class MyUnitTests (TestCase) :
-    def setUp (self) :
-        self.a = [
+class MyUnitTests (TestCase) : #class derived from test case
+    def setUp (self) : # run every time you set up the unit test
+        self.a = [ # alias them in a list and iterate through instead of writing a test for each one
             reduce_for_range,
             reduce_for,
             reduce_while,
             reduce]
 
-    def test_1 (self) :
-        for f in self.a :
-            with self.subTest() :
+    def test_1 (self) : # one unit test
+        for f in self.a : #iterate through the functions
+            with self.subTest() : #keep going if failure
                 self.assertEqual(f(None, [],       0),  0)
 
     def test_2 (self) :
         for f in self.a :
             with self.subTest() :
-                self.assertEqual(f(add, [2],       1),  3)
+                self.assertEqual(f(add, [2],       1),  3) # start at 1, add 2 = 3
 
     def test_3 (self) :
         for f in self.a :
             with self.subTest() :
-                self.assertEqual(f(add, [2, 3, 4], 0),  9)
+                self.assertEqual(f(add, [2, 3, 4], 0),  9) # start at 0 add 2, 2+3 = 5, 5+4 = 9
 
     def test_4 (self) :
         for f in self.a :
