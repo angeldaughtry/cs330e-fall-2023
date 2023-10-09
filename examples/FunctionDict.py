ede58536f5791851b9554dea20ce522f5a20dda7
angeldaughtry <100389979+angeldaughtry@users.noreply.github.com>
2023-10-09 12:16:27 -0500

    Update FunctionDict.py

--- examples/FunctionDict.py
+++ examples/FunctionDict.py
@@ -10,14 +10,14 @@ def f (x, y, **z) :
     return [x, y, z]
 
 assert f(2, 3)               == [2, 3, {}]
-assert f(2, 3, a = 4)        == [2, 3, {'a' : 4}]
+assert f(2, 3, a = 4)        == [2, 3, {'a' : 4}]  # must assign the key before dict packing
 assert f(2, 3, a = 4, b = 5) == [2, 3, {'a' : 4, 'b' : 5}]
 
 d1 = {"b" : 4, "a" : 3}
 assert f(2, 5,     **d1)  == [2, 5, {'a' : 3, 'b' : 4}]
 assert f(2, y = 5, **d1)  == [2, 5, {'a' : 3, 'b' : 4}]
 u = (2,)
-assert f(y = 5, *u, **d1) == [2, 5, {'a' : 3, 'b' : 4}]
+assert f(y = 5, *u, **d1) == [2, 5, {'a' : 3, 'b' : 4}] # order of unpacking is x = u, y=5, then dict unpacked (repacked in the function)
 assert f(*u, y = 5, **d1) == [2, 5, {'a' : 3, 'b' : 4}]
 
 d2 = {"y" : 3, "x" : 2}
