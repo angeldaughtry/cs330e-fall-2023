e554ad3aeff7c434783645c212aba429db962f61
angeldaughtry <100389979+angeldaughtry@users.noreply.github.com>
2023-10-09 11:33:03 -0500

    Notes Comprehensions.py

--- examples/Comprehensions.py
+++ examples/Comprehensions.py
@@ -45,7 +45,7 @@ assert list(y) == []
 
 x = [2, 3, 4]
 y = (v * 5 for v in x)
-x += [5]
+x += [5] #point is to see what happens to the generator if you change the old list
 assert x       == [ 2,  3,  4,  5]
 assert list(y) == [10, 15, 20, 25]
 assert list(y) == []
@@ -64,7 +64,7 @@ assert list(y) == []
 x = [2, 3, 4, 5, 6]
 y = []
 for v in x :
-    if v % 2 :
+    if v % 2 : # if its odd = 0 which in python is the same thing as true
         y += [v * 5]
 assert x == [2,  3,  4,  5,  6]
 assert y == [   15,     25]
@@ -81,7 +81,7 @@ assert list(y) == [    15,     25]
 assert list(y) == []
 
 x = [2, 3, 4, 5, 6]
-y = filter(lambda v : v % 2, x)
+y = filter(lambda v : v % 2, x) # generator that generates the odd values
 assert isinstance(y, filter)
 assert hasattr(y, "__next__")
 assert hasattr(y, "__iter__")
@@ -93,7 +93,7 @@ assert list(z) == []
 x = [2, 3, 4]
 y = [4, 5]
 z = []
-for v in x :
+for v in x : #nested loop
     for w in y :
         z += [v + w]
 assert x == [2, 3, 4]
@@ -103,7 +103,7 @@ assert z == [  6,   7,   7,   8,   8,   9]
 
 x = [2, 3, 4]
 y = [4, 5]
-z = [v + w for v in x for w in y]
+z = [v + w for v in x for w in y] #same as the nested loop
 assert x == [2, 3, 4]
 assert y == [4, 5]
 assert z == [2+4, 2+5, 3+4, 3+5, 4+4, 4+5]
@@ -111,7 +111,7 @@ assert z == [  6,   7,   7,   8,   8,   9]
 
 x = [2, 3, 4]
 y = [4, 5]
-z = (v + w for v in x for w in y)
+z = (v + w for v in x for w in y) #same as previous but using a generator
 assert x       == [2, 3, 4]
 assert y       == [4, 5]
 assert list(z) == [2+4, 2+5, 3+4, 3+5, 4+4, 4+5]
@@ -120,12 +120,12 @@ assert list(z) == []
 x = {2, 3, 4}
 y = set()
 for v in x :
-    y |= {v * 5}
+    y |= {v * 5} # |= like appending but does not allow duplicates
 assert x == { 2,  3,  4}
 assert y == {10, 15, 20}
 
 x = {2, 3, 4}
-y = {v * 5 for v in x}   # set comprehension
+y = {v * 5 for v in x}   # set comprehension (same as list comprehension
 assert x == { 2,  3,  4}
 assert y == {10, 15, 20}
 
@@ -145,33 +145,33 @@ assert x == {2 : "abc",    3 : "def",    4 : "ghi"}
 assert y == {3 : "abcxyz", 4 : "defxyz", 5 : "ghixyz"}
 
 a = [2, 3, 4]
-r = reversed(a)
+r = reversed(a) #generator
 assert str(type(r)) == "<class 'list_reverseiterator'>"
 assert list(r) == [4, 3, 2]
 assert list(r) == []
 
 a = ["abc", "def", "ghi"]
-e = enumerate(a)
+e = enumerate(a) # creates a list of tuples, also a generator
 assert str(type(e)) == "<class 'enumerate'>"
 assert list(e) == [(0, "abc"), (1, "def"), (2, "ghi")]
 assert list(e) == []
 
 a = [2, 3, 4]
 b = [5, 6, 7]
-z = zip(a, b)
+z = zip(a, b) # combines corresponding elements and tuples and is also a generator
 assert str(type(z)) == "<class 'zip'>"
 assert list(z) == [(2, 5), (3, 6), (4, 7)]
 assert list(z) == []
 
 a = [4, 2, 3]
-s = sorted(a)
+s = sorted(a) # sorts elements in order but not a generator
 assert a == [4, 2, 3]
 assert s == [2, 3, 4]
 
 class A :
     pass
 
-assert all([])
+assert all([]) #True, base case
 assert all([
     A(),
     True,
@@ -181,13 +181,13 @@ assert all([
     [2, 3, 4],
     (2, 3, 4),
     {2, 3, 4},
-    {2 : "abc", 3 : "def", 4 : "ghi"}])
+    {2 : "abc", 3 : "def", 4 : "ghi"}]) # if you pass elements in all, it'll be true if all of the elements inside the list is true
 
 class B :
     def __bool__ (self) :
         return False
 
-assert not any([])
-assert not any([B(), False, 0, 0.0,  "", [], (), set(), {}])
+assert not any([]) # true if any of its components is true, so "not" means I don't want any of them to be true
+assert not any([B(), False, 0, 0.0,  "", [], (), set(), {}]) # all elements are false
 
 print("Done.")
